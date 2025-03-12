707b82ba3b601bfba7600ee9847c369a85d3094d
Jenya Kalinin <evgeny.k@changenow.io>
2025-03-12 23:37:47 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './types';
+export * from './coin.service';
+export * from './node-adapter';
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import {
+  TransactionBroadcastParams,
+  TransactionBroadcastResults,
+  TransactionParams,
+} from '../common';
+
+export type <%- networkCap %>TransactionParams = TransactionParams;
+
+export type <%- networkCap %>TransactionBroadcastParams = TransactionBroadcastParams;
+
+export type <%- networkCap %>TransactionBroadcastResults = TransactionBroadcastResults;
