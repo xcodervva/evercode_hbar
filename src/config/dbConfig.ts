d7606783828650018ddc175364eaccf1926b2de2
VVoronkov <voronglass84@gmail.com>
2025-11-07 22:17:18 +0300

    Add dbConfig

---
+++ src/config/dbConfig.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || 'your_user',
+    password: process.env.DB_PASSWORD || 'your_password',
+    database: process.env.DB_NAME || 'your_database',
+};
